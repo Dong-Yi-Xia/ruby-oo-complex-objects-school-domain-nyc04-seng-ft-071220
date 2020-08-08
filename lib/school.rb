e28e60385c5cc9adf6870342c63bd5191839bf1a
Dong Xia <dong7777xia@gmail.com>
2020-08-08 22:17:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+# code here!
+require 'pry'
+
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+         if !self.roster[grade]
+            self.roster[grade] = []
+         end
+        self.roster[grade]<<student_name
+    end
+
+    def grade(lvl)
+        self.roster[lvl]
+    end
+
+    def sort
+        self.roster.each do |grade, student|
+            self.roster[grade] = student.sort
+        end
+    end
+
+
+end
